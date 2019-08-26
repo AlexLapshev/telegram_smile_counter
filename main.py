f9d252e4336cf76578b0e4a1b00315d1848d1db1
AlexLapshev <lovemusicornot@gmail.com>
2019-08-26 21:31:44 +0300

    added inclination

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ search = ':('
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+
 	if search in message.text:
 		num = message.text.count(search)
 		user_id = message.from_user.id
@@ -17,7 +18,13 @@ def get_text_messages(message):
 		bot.reply_to(message, 'Опять грустим?')
 		dict_of_sadness.setdefault(user_id, 0)
 		dict_of_sadness[user_id] += num
-		bot.send_message(message.chat.id, '@{} вы уже грустили {} раза.'.format(username, dict_of_sadness[user_id]))
+		if str(dict_of_sadness[user_id])[len(str(dict_of_sadness[user_id])) - 2:] in \
+				['1', '5', '6', '7', '8', '9', '11', '12', '13', '14', '15', '16', '17', '18', '19']\
+				or str(dict_of_sadness[user_id])[-1] == '0':
+			word = 'раз'
+		else:
+			word = 'раза'
+		bot.send_message(message.chat.id, '@{} Вы уже грустили {} {}.'.format(username, dict_of_sadness[user_id], word))
 
 
 if __name__ == '__main__':
